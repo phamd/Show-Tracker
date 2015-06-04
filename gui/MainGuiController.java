731ff9afd55846c1239bc4464a4de0bc91ea6c0a
Don Pham <dpham@live.ca>
2015-06-03 22:54:25 -0400

    Changes: about.fxml removed and replaced with an Alert popup. Retabbed and recommented some files.

--- gui/MainGuiController.java
+++ gui/MainGuiController.java
@@ -1,16 +1,17 @@
 package gui;
 
+import gui.MainGui;
+import gui.NewShowTabController;
+
 import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -18,28 +19,22 @@ import show.Show;
 import show.ShowList;
 
 public class MainGuiController {
-//	private Stage stage;
-//	private BorderPane layout;
-//	private Scene scene;
-    
-	@FXML private MenuItem menuAbout;
-	@FXML private MenuItem menuAdd;
+
 	@FXML private VBox showListBox;
 	@FXML private ScrollPane scrollPane;
+	@FXML private MenuItem menuAdd;
 	@FXML private MenuItem menuSave;
 	@FXML private MenuItem menuLoad;
+	@FXML private MenuItem menuAbout;
+	@FXML private MenuItem menuInstructions;
 	
     private ShowList showList;
-	private BorderPane rootLayout;
 	
 	@FXML
 	void initialize() {
 		
 		showList = new ShowList();
 		
-		menuAbout.setOnAction(event -> {
-			openMenuStage("about.fxml");
-		});
 		menuAdd.setOnAction(event -> {
 			makeNewShow();
 		});
@@ -50,35 +45,54 @@ public class MainGuiController {
 			loadShows();
 		});
 		
+		menuInstructions.setOnAction(event -> {
+//			openMenuStage("instructions.fxml");
+			System.out.println("No instructions");
+		});
+		
+		menuAbout.setOnAction(event -> {
+			openMenuAbout();
+		});
+		
 //		showListBox.setFillWidth(true);
 		scrollPane.setFitToHeight(true);
 		scrollPane.setFitToWidth(true);
 		
 		loadShows();
-		
-	// add more buttons here
 	}
-	
+
 	private void openMenuStage(String fxmlFile) {
 		 try {
 	    	FXMLLoader loader = new FXMLLoader();
 	    	loader.setLocation(MainGui.class.getResource(fxmlFile));
-//	        	Parent root = FXMLLoader.load(getClass(	).getResource("about.fxml"));
+//	        Parent root = FXMLLoader.load(getClass(	).getResource("about.fxml"));
 	    	Region layout = loader.load();
-	        Stage stage = new Stage();
 	        Scene scene = new Scene(layout);
+	        Stage stage = new Stage();
 	        stage.setScene(scene);
-//	            stage.setTitle("My New Stage Title");
-//	            stage.setScene(new Scene(root, 450, 450));
+//	        stage.setTitle("My New Stage Title");
+//	        stage.setScene(new Scene(root, 450, 450));
 	        stage.show();
 	
-	        //hide this current window (if this is what you want
-	        //((Node)(event.getSource())).getScene().getWindow().hide();
+//	         hide this current window
+//	        ((Node)(event.getSource())).getScene().getWindow().hide();
 	
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+	
+	private void openMenuAbout() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About");
+		alert.setHeaderText("Welcome to Show Tracker.");
+		alert.setContentText("Show Tracker keeps track of numbering information " +
+							 "for things such as shows, movies, manga, comics, " +
+							 "and books. \n\nThis program was made by: " +
+							 "Don Pham & Steven Luu.");
+		alert.show();
+	}
+	
 	
 	private void makeNewShow() {
 		 try {
@@ -101,16 +115,16 @@ public class MainGuiController {
 			 controller.setShowList(showList);
 			 controller.setParentNode(showListBox);
 			 
-			 Region newshowtab = loader.load();
-			 controller.setThisNode(newshowtab);
+			 Region newShowTab = loader.load();
+			 controller.setThisNode(newShowTab);
 			 
-			 /* // set defaults
-			 ((TextField) newshowtab.lookup("#showNameTxt")).setText(newShow.getName());
-			 ((TextField) newshowtab.lookup("#seasonTxt")).setText(Integer.toString(newShow.getSeasonNumber()));
-			 ((TextField) newshowtab.lookup("#episodeTxt")).setText(Integer.toString(newShow.getSeasonNumber()));
-			 ((TextField) newshowtab.lookup("#descriptionTxt")).setText(newShow.getDescription());
+			 /* // set default field text
+			 ((TextField) newShowTab.lookup("#showNameTxt")).setText(newShow.getName());
+			 ((TextField) newShowTab.lookup("#seasonTxt")).setText(Integer.toString(newShow.getSeasonNumber()));
+			 ((TextField) newShowTab.lookup("#episodeTxt")).setText(Integer.toString(newShow.getSeasonNumber()));
+			 ((TextField) newShowTab.lookup("#descriptionTxt")).setText(newShow.getDescription());
 			 */
-			 showListBox.getChildren().add(newshowtab);
+			 showListBox.getChildren().add(newShowTab);
 		 } catch (Exception e) {
 			 e.printStackTrace();
 		 }
@@ -132,13 +146,4 @@ public class MainGuiController {
 	public void setShowList(ShowList showList) {
 		this.showList = showList;
 	}
-    
-	public void setRootLayout(BorderPane layout) {
-		this.rootLayout = layout;
-  	}
-    
-//	  public void setStage(Stage stage) { this.stage = stage; System.out.println("tets1");}
-//    public void setScene(Scene scene) { this.scene = scene; System.out.println("tets2");}
-
-
 }
--- gui/NewShowTabController.java
+++ gui/NewShowTabController.java
@@ -1,14 +1,14 @@
 package gui;
 
+import show.Show;
+import show.ShowList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import show.Show;
-import show.ShowList;
 
 public class NewShowTabController {
 
